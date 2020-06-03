aa2f71a67169a6f6e8f36f840b4300c99f4be7c9
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-06-03 16:07:30 +0000

    folder structure set up

---
+++ src/pages/cv/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const index = () => {
+  return <div>This will be the cv page</div>
+}
+
+export default index
---
+++ src/pages/portfolio/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const index = () => {
+  return <div>This will be the portfolio site</div>
+}
+
+export default index
