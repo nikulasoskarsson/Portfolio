af7d7f19368be1be24fd1830bec5156ff1835261
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-06-29 19:33:50 +0000

    getting form data from prismic

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,7 +1,29 @@
 import React from "react"
+import Layout from "../../components/layout/layout"
+import { graphql } from "gatsby"
 
-const index = () => {
-  return <div>This will be the contact page</div>
+export const query = graphql`
+  {
+    prismic {
+      allContact_pages {
+        edges {
+          node {
+            form_title
+            form_fields {
+              form_label
+              form_placeholder
+              form_type
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const index = ({ data }) => {
+  console.log("contact data", data)
+  return <Layout>This will be the contact page</Layout>
 }
 
 export default index
