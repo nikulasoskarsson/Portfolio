ed14b1f19883bd31d1ae25c2d73b1bf99ebf8b10
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-08-09 09:56:02 +0000

    querying for portfolio items from prismic

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,8 +1,48 @@
 import React from "react"
 import Layout from "../../components/layout/layout"
+import { graphql } from "gatsby"
 import Dropdown from "./components/filters/dropdown/dropdown"
 
-const index = () => {
+export const query = graphql`
+  {
+    prismic {
+      allPortfoliopages {
+        edges {
+          node {
+            body {
+              ... on PRISMIC_PortfoliopageBodyPortfolio_section {
+                type
+                label
+                fields {
+                  description
+                  image
+                  github_link {
+                    ... on PRISMIC__ExternalLink {
+                      url
+                    }
+                  }
+                  site_link {
+                    ... on PRISMIC__ExternalLink {
+                      url
+                    }
+                  }
+                  stack
+                  title
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const index = ({ data }) => {
+  console.log(
+    "portoflio data",
+    data.prismic.allPortfoliopages.edges[0].node.body[0].fields
+  )
   return (
     <Layout>
       <div style={{ padding: 60 }}>
