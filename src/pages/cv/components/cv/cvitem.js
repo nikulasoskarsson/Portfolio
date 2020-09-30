77f304ee178e8a9200351da09b2209629443acd8
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-09-30 19:54:07 +0200

    refactored conditinal rendering since it was causing an error in netlify

--- src/pages/cv/components/cv/cvitem.js
+++ src/pages/cv/components/cv/cvitem.js
@@ -16,29 +16,33 @@ const CvItem = ({ item }) => {
       <StyledSmallCircle />
       <div style={{ flex: 0.6 }}>
         <StyledMediumText>
-          {item.company_name || item.school_name}
+          {item.company_name ? item.company_name : item.school_name}
         </StyledMediumText>
-        <StyledLightText>{item.date || item.education_date}</StyledLightText>
+        <StyledLightText>
+          {item.date ? item.date : item.education_date}
+        </StyledLightText>
       </div>
       <div style={{ flex: 1, alignSelf: "start" }}>
         <StyledMediumText>
-          {item.job_title || item.program_name}
+          {item.job_title ? item.job_title : item.program_name}
         </StyledMediumText>
         <StyledLightText>
-          {item.job_description || item.program_description}lorem lorem lorem
-          lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Suscipit culpa quas dicta error iusto, corporis facere sequi
-          accusantium neque illo odit beatae vero, earum veniam aperiam, cum
-          orem lorem lorem Lorem ipsum dolor sit, amet consectetur adipisicing
-          elit. Suscipit culpa quas dicta error iusto, corporis facere sequi
-          accusantium neque illo odit beatae vero, earum veniam aperiam, cum
-          orem lorem lorem Lorem ipsum dolor sit, amet consectetur adipisicing
-          elit. Suscipit culpa quas dicta error iusto, corporis facere sequi
-          accusantium neque illo odit beatae vero, earum veniam aperiam, cum
-          orem lorem lorem Lorem ipsum dolor sit, amet consectetur adipisicing
-          elit. Suscipit culpa quas dicta error iusto, corporis facere sequi
-          accusantium neque illo odit beatae vero, earum veniam aperiam, cum
-          perspiciatis id? Ex.s
+          {item.job_description
+            ? item.job_description
+            : item.program_description}
+          lorem lorem lorem lorem Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Suscipit culpa quas dicta error iusto, corporis
+          facere sequi accusantium neque illo odit beatae vero, earum veniam
+          aperiam, cum orem lorem lorem Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Suscipit culpa quas dicta error iusto, corporis
+          facere sequi accusantium neque illo odit beatae vero, earum veniam
+          aperiam, cum orem lorem lorem Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Suscipit culpa quas dicta error iusto, corporis
+          facere sequi accusantium neque illo odit beatae vero, earum veniam
+          aperiam, cum orem lorem lorem Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Suscipit culpa quas dicta error iusto, corporis
+          facere sequi accusantium neque illo odit beatae vero, earum veniam
+          aperiam, cum perspiciatis id? Ex.s
         </StyledLightText>
       </div>
     </StyledCvItem>
