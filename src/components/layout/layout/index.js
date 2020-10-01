97a73c32b929d4c88c243801baa251cf9ea851c1
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-10-01 10:04:43 +0200

    animation stuff and warning fixes

--- src/components/layout/layout/index.js
+++ src/components/layout/layout/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 import Header from "../header"
 import "../../layout.css"
 import { PageVariants } from "../../../variants"
@@ -13,15 +12,15 @@ import StyledLayout from "../../styled-components/StyledLayout"
 const Layout = ({ children }) => {
   const theme = useTheme()
 
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
 
   return (
     <ThemeProvider theme={theme}>
--- src/pages/portfolio/portfolio/portfoliolist.js
+++ src/pages/portfolio/portfolio/portfoliolist.js
@@ -1,13 +1,26 @@
 import React from "react"
 import PortfolioItem from "./portfolioitem"
 import { StyledGridContainer } from "../../../components/styled-components/grid/"
+import { motion } from "framer-motion"
 
 const PortfolioList = ({ data }) => {
   return (
     <StyledGridContainer>
-      {data.map(item => (
-        <PortfolioItem item={item} />
-      ))}
+      {data.map((item, index) => {
+        return (
+          <motion.div
+            key={index}
+            initial={{
+              opacity: 0,
+              translateY: index % 2 === 0 ? "8%" : "-8%",
+            }}
+            animate={{ opacity: 1, translateY: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+          >
+            <PortfolioItem item={item} />
+          </motion.div>
+        )
+      })}
     </StyledGridContainer>
   )
 }
