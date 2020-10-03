541d1598dfa24448ed8511e9e6eeeff103c68ce8
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-10-03 10:56:06 +0200

    queries not working

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -12,53 +12,58 @@ import {
 import { useDispatch } from "react-redux"
 import { setTheme } from "../../../theme/actions"
 
-const query = graphql`
-  {
-    prismic {
-      allNavigations {
-        edges {
-          node {
-            branding
-            navigation_links {
-              navigation_label
-              navigation_link
-            }
-          }
-        }
-      }
-    }
-  }
-`
+// const query = graphql`
+//   {
+//     prismic {
+//       allNavigations {
+//         edges {
+//           node {
+//             branding
+//             navigation_links {
+//               navigation_label
+//               navigation_link
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
 
 const Header = props => {
   const dispatch = useDispatch()
   return (
-    <StaticQuery
-      query={query}
-      render={data => {
-        return (
-          <StyledNav>
-            <StyledNavContainer>
-              <StyledNavBranding to="/homepage">
-                {data.prismic.allNavigations.edges[0].node.branding}
-              </StyledNavBranding>
-              <StyledNavList>
-                {data.prismic.allNavigations.edges[0].node.navigation_links.map(
-                  (navigation_link, index) => (
-                    <StyledNavListItem key={index}>
-                      <StyledNavLink to={navigation_link.navigation_link}>
-                        {navigation_link.navigation_label}
-                      </StyledNavLink>
-                    </StyledNavListItem>
-                  )
-                )}
-                <button onClick={() => dispatch(setTheme())}>theme</button>
-              </StyledNavList>
-            </StyledNavContainer>
-          </StyledNav>
-        )
-      }}
-    />
+    <div>
+      {" "}
+      <h3>Testing</h3>
+    </div>
+
+    // {/* <StaticQuery
+    //   query={query}
+    //   render={data => {
+    //     return (
+    //       <StyledNav>
+    //         <StyledNavContainer>
+    //           <StyledNavBranding to="/homepage">
+    //             {data.prismic.allNavigations.edges[0].node.branding}
+    //           </StyledNavBranding>
+    //           <StyledNavList>
+    //             {data.prismic.allNavigations.edges[0].node.navigation_links.map(
+    //               (navigation_link, index) => (
+    //                 <StyledNavListItem key={index}>
+    //                   <StyledNavLink to={navigation_link.navigation_link}>
+    //                     {navigation_link.navigation_label}
+    //                   </StyledNavLink>
+    //                 </StyledNavListItem>
+    //               )
+    //             )}
+    //             <button onClick={() => dispatch(setTheme())}>theme</button>
+    //           </StyledNavList>
+    //         </StyledNavContainer>
+    //       </StyledNav> */}
+    //     )
+    //   }}
+    // />
   )
 }
 
--- src/pages/cv/index.js
+++ src/pages/cv/index.js
@@ -3,39 +3,39 @@ import Layout from "../../components/layout/layout"
 import { graphql } from "gatsby"
 import CvSection from "./components/cv/cvsection"
 
-export const query = graphql`
-  {
-    prismic {
-      allCv_pages {
-        edges {
-          node {
-            work_experience {
-              company_name
-              date
-              job_description
-              job_title
-            }
-            education_title
-            work_experience_title
-            education {
-              education_date
-              program_description
-              program_name
-              school_name
-            }
-          }
-        }
-      }
-    }
-  }
-`
-const index = ({ data }) => {
-  const { work_experience, education } = data.prismic.allCv_pages.edges[0].node
+// export const query = graphql`
+//   {
+//     prismic {
+//       allCv_pages {
+//         edges {
+//           node {
+//             work_experience {
+//               company_name
+//               date
+//               job_description
+//               job_title
+//             }
+//             education_title
+//             work_experience_title
+//             education {
+//               education_date
+//               program_description
+//               program_name
+//               school_name
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
+const index = () => {
+  // const { work_experience, education } = data.prismic.allCv_pages.edges[0].node
 
   return (
     <Layout>
-      <CvSection title="Work experience" list={work_experience} />
-      <CvSection title="Education" list={education} />
+      {/* <CvSection title="Work experience" list={work_experience} />
+      <CvSection title="Education" list={education} /> */}
     </Layout>
   )
 }
